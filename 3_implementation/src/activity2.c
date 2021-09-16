c7bf7f0dbde934e1ce0ab51fead9e01c92f6bc4e
prathipati supriya <supriyaprathipati9@gmail.com>
2021-09-16 17:58:09 +0530

    update

--- 3_implementation/src/activity2.c
+++ 3_implementation/src/activity2.c
@@ -1,3 +1,19 @@
+/**
+ * @file activity2.h
+ * @author supriya (you@domain.com)
+ * @brief  Read sensor analog values and convert it into digital values using ADC
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+/**
+ * @brief initialize the ADC (Analog to Digital Converter)
+ * 
+ */
 void InitADC()
 {
     ADMUX=(1<<REFS0);
